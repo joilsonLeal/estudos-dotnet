36b39e9dd033cbc076dcf19f5b093188f40091ec
= <=>
2020-08-15 16:52:26 -0300

    feature: aplicação do primeiro mapper

--- AutoMapper/01/Program.cs
+++ AutoMapper/01/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using AutoMapper;
 
 namespace mapper
 {
@@ -6,7 +7,32 @@ namespace mapper
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var configuration = new MapperConfiguration(cfg =>
+            {
+                // origem , destino
+                cfg.CreateMap<FooDto, Foo>();
+            });
+
+            var mapper = configuration.CreateMapper();
+
+            var fooDto = new FooDto()
+            {
+                Name = "fernando",
+            };
+
+            var foo = mapper.Map<Foo>(fooDto);
+
+            Console.WriteLine(foo.Name);
         }
+    }
+
+    class FooDto
+    {
+        public string Name { get; set; }
+    }
+
+    class Foo
+    {
+        public string Name { get; set; }
     }
 }
