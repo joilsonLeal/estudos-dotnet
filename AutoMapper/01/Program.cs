6c867b2f08e96bfc59d560a156e8931e41de25f9
Joilson <joilson_rg@hotmail.com>
2020-08-15 15:42:34 +0000

    chore: criação de repositório

---
+++ AutoMapper/01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace mapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
