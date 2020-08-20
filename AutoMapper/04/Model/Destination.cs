c004ca26777120c61935f9cb7065185595e5fa06
joilson <joilson_rg@hotmail.com>
2020-08-20 09:03:50 -0300

    feature: adiciona estrutura e converte string para diferentes tipos

---
+++ AutoMapper/04/Model/Destination.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _02.Model
+{
+    public class Destination
+    {
+        public int Value1 { get; set; }
+        public DateTime Value2 { get; set; }
+        public Type Value3 { get; set; }
+    }
+}
---
+++ AutoMapper/04/Pages/Index.cshtml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using _02.Model;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace _02.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly IMapper _mapper;
+
+        public IndexModel(ILogger<IndexModel> logger, IMapper mapper)
+        {
+            _logger = logger;
+            _mapper = mapper;
+        }
+
+        public async Task OnGetAsync()
+        {
+
+            var source = new Source
+            {
+                Value1 = "5",
+                Value2 = "20/08/2020",
+                Value3 = "System.Linq"
+            };
+            try
+            {
+                Destination result = _mapper.Map<Source, Destination>( source );
+
+            }
+            catch( Exception e )
+            {
+
+                throw;
+            }
+        }
+    }
+}
